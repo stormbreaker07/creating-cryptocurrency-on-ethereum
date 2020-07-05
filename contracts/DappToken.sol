330a7011f824c5c7ab531c2b48fb94ef2588a22e
stormbreaker07 <tanuj07yadav07@gmail.com>
2020-07-05 05:46:13 +0530

    successfully created transfer function

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -8,9 +8,36 @@ contract DappToken {
 	// set the token number of token
 	// read the total number of token
 	uint256 public totalSupply;
+	string public name = 'DApp Token';
+	string public symbol = 'DAPP';
+	string public standard = 'DApp token v1.0';
 
-  constructor() public {
-  	totalSupply = 1000000;
-    
+	mapping (address => uint256) public balanceOf;
+
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+		); 
+	
+
+  constructor(uint256 _initialSupply) public {
+  	balanceOf[msg.sender] = _initialSupply;
+  	totalSupply = _initialSupply;
   }
+
+ //transfer function   
+
+function transfer (address _to, uint256 _value) public returns(bool success) {
+	//eception if amount is not enough
+	require(balanceOf[msg.sender] >= _value);
+	balanceOf[msg.sender] -= _value;
+	balanceOf[_to] += _value;
+	//transfer event
+	Transfer(msg.sender , _to , _value);
+	//retrun a boolean
+}
+
+
+
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -2,12 +2,52 @@ const DappToken = artifacts.require("./DappToken.sol");
 
 
 contract('DappToken', function(accounts) {
+	
+	var tokenInstance;
+
+	it('initialize the contract with correct values' , function() {
+		return DappToken.deployed().then(function(instance){
+			tokenInstance = instance;
+			return tokenInstance.name();
+		}).then(function(name) {
+			assert.equal(name , 'DApp Token' , 'correct name')
+			return tokenInstance.symbol();
+		}).then(function(symbol){
+			assert.equal(symbol, 'DAPP' ,'correct symbol')
+			return tokenInstance.standard();
+		}).then(function(standard){
+			assert.equal(standard , 'DApp token v1.0' , 'correct standard')
+		})
+	})
+
 	it('set the totalSupply upon development', function() {
 		return DappToken.deployed().then(function(instance) {
 			tokenInstance= instance;
 			return tokenInstance.totalSupply();
 		}).then(function(totalSupply) {
 			assert.equal(totalSupply.toNumber(),1000000,'set the total supply to 1000000');
+			return tokenInstance.balanceOf(accounts[0]);	
+		}).then(function(adminBalance) {
+			assert.equal(adminBalance.toNumber() , 1000000 , 'it allocates inital supply to admin account')
 		});
 	});
+
+
+	it('transfer token ownership', function() {
+		return DappToken.deployed().then(function(instance) {
+			tokenInstance = instance;
+		return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
+		}).then(assert.fail).catch(function(error) {
+			assert(error.message.indexOf('revert') >= 0 , 'error message must contain revert');
+			return tokenInstance.transfer(accounts[1],250000 , {from: accounts[0] });
+		}).then(function(receipt) {
+			return tokenInstance.balanceOf(accounts[1]);
+		}).then(function(balance) {
+			assert.equal(balance.toNumber(),250000 , 'add amount to recieving account');
+			return tokenInstance.balanceOf(accounts[0]);
+		}).then(function(balance) {
+			assert.equal(balance.toNumber() , 750000 , 'deduct the amount from sending account');
+		})
+	});
+
 }) 
